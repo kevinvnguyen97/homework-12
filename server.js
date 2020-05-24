57a045a41b7509d854f539f379e6fd6efbd17e74
ThaNinjaPower <kevinvannguyen1997@gmail.com>
2020-05-24 14:38:56 -0500

    Added demo video

--- server.js
+++ server.js
@@ -1,19 +1,25 @@
 var mysql = require("mysql");
 var mainPrompt = require("./prompts/mainPrompt");
 
-var connection = mysql.createConnection({
-  host: "localhost",
+var connection;
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+}
+else {
+  connection = mysql.createConnection({
+    host: "localhost",
 
-  // Your port; if not 3306
-  port: 3306,
+    // Your port; if not 3306
+    port: 3306,
 
-  // Your username
-  user: "root",
+    // Your username
+    user: "root",
 
-  // Your password
-  password: "root",
-  database: "employee_db"
-});
+    // Your password
+    password: "root",
+    database: "employee_db"
+  });
+}
 
 connection.connect(function (err) {
   if (err) throw err;
